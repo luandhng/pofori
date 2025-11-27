e6bce285564c63aa9dc37c9cd48dae71a7871c04
Luan <97277916+luandhng@users.noreply.github.com>
2025-11-27 09:18:17 -0800

    update

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,8 +1,16 @@
 const Hero = () => {
   return (
-    <div className="max-w-7xl mx-auto py-32">
-      <h1 className="text-3xl">Build your portfolio in minutes.</h1>
-      <h1 className="text-3xl">Pofori is standard way for your portfolio.</h1>
+    <div className="max-w-5xl mx-auto py-20 flex flex-col gap-6 font-medium">
+      <div className="flex flex-col gap-1 tracking-tight">
+        <h1 className="text-2xl">Build your portfolio in minutes.</h1>
+        <h1 className="text-2xl">Pofori is standard way for your portfolio.</h1>
+      </div>
+
+      <div>
+        <button className="text-base cursor-pointer bg-black text-white rounded-full leading-0 py-5 px-7">
+          Start your site
+        </button>
+      </div>
     </div>
   );
 };
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,12 +3,12 @@ import { Button } from "./ui/button";
 export const Navbar = () => {
   return (
     <div className="">
-      <nav className="max-w-7xl flex font-medium items-center justify-between mx-auto py-4">
+      <nav className="max-w-5xl relative flex font-medium items-center justify-between mx-auto py-4">
         <div className="flex gap-8 items-center">
           <h1 className="font-bold text-xl">Pofori</h1>
         </div>
 
-        <div className="flex gap-8">
+        <div className="flex gap-8 absolute left-1/2 -translate-x-1/2">
           <a href="">Pricing</a>
           <a href="">Blog</a>
           <a href="">Changelog</a>
