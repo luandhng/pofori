b3072628f463d1104d1095e69f5e633123f747c6
Luan <97277916+luandhng@users.noreply.github.com>
2025-11-27 06:46:00 -0800

    update

---
+++ components/hero.tsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+  return (
+    <div className="max-w-7xl mx-auto py-32">
+      <h1 className="text-3xl">Build your portfolio in minutes.</h1>
+      <h1 className="text-3xl">Pofori is standard way for your portfolio.</h1>
+    </div>
+  );
+};
+
+export default Hero;
