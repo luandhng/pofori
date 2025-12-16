e3b9006bfe6bc9f88068492bcdbdf2287c38ee8c
Luan <97277916+luandhng@users.noreply.github.com>
2025-12-16 15:16:15 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import { Hero } from "@/components/landing-page/Hero";
-import { Nav } from "@/components/landing-page/Nav";
-
 export default function Home() {
-  return (
-    <div className="max-w-7xl mx-auto">
-      <Nav />
-      <Hero />
-    </div>
-  );
+  return <div></div>;
 }
